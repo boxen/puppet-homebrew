ad98dc51851a75809ce42e7543bd853be871df14
Will Farrington <wfarr@github.com>
2012-10-18 15:35:53 -0700

    really fix gyp formula

--- files/brews/gyp.rb
+++ files/brews/gyp.rb
@@ -1,21 +1,25 @@
-# Adapted from https://github.com/mxcl/homebrew/pull/11776/files
-
 require 'formula'
 
 class Gyp < Formula
   homepage 'http://code.google.com/p/gyp/'
-  url 'http://gyp.googlecode.com/svn/trunk', :revision => 1518
-  version 'trunk-1518-boxen1'
   head 'http://gyp.googlecode.com/svn/trunk'
 
+  depends_on 'scons'
+
   def install
+    system "mkdir -p #{prefix}"
+    system "mkdir -p #{libexec}"
+    system "mkdir -p #{bin}"
+    # This is very similar to the Duplicity formular.
+    # Install mostly into libexec
     system "python", "setup.py", "install",
-      "--prefix=#{prefix}", "--install-purelib=#{libexec}",
-      "--install-platlib=#{libexec}", "--install-scripts=#{bin}"
+                     "--prefix=#{prefix}",
+                     "--install-purelib=#{libexec}",
+                     "--install-platlib=#{libexec}",
+                     "--install-scripts=#{bin}"
 
-    mv bin + 'gyp', bin + 'gyp.py'
-    mv Dir[bin + '*'], libexec
-
+    # Shift files around to avoid needing a PYTHONPATH
+    mv bin+'gyp', "#{libexec}/gyp.py"
     bin.install_symlink "#{libexec}/gyp.py" => "gyp"
   end
 end
