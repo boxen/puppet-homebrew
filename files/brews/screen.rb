8dfef97b1b49ddd31e9bbf6069ae86831b7d73d8
Simon Rozet <simon@rozet.name>
2012-11-23 12:01:45 +0100

    follow version convention for screen brew

--- files/brews/screen.rb
+++ files/brews/screen.rb
@@ -7,7 +7,7 @@ class Screen <Formula
   url 'http://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz'
   homepage 'http://www.gnu.org/software/screen'
   md5 '8506fd205028a96c741e4037de6e3c42'
-  version '4.00.03'
+  version '4.00.03-boxen1'
 
   head 'git://git.savannah.gnu.org/screen.git', :branch => 'master'
 
