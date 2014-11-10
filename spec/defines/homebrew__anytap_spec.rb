fa77d6f993f7b570f22ae721be9d65397ab80bd6
xiaogaozi <gaochangjian@gmail.com>
2014-11-10 22:24:41 +0800

    more matcher

--- spec/defines/homebrew__anytap_spec.rb
+++ spec/defines/homebrew__anytap_spec.rb
@@ -1,23 +1,35 @@
 require "spec_helper"
 
 describe "homebrew::anytap" do
-  let(:facts) { default_test_facts }
+  title = "foo/bar"
+  url = "git@git.example.com:foo/homebrew-bar.git"
 
-  let(:title) { "foo/bar" }
+  let(:facts) { default_test_facts }
+  let(:title) { title }
 
   context "ensure => present" do
     let(:params) do
       {
         :ensure => "present",
-        :url    => "git@git.example.com:foo/homebrew-bar.git"
+        :url    => url
       }
     end
 
     it do
-      should contain_homebrew__anytap("foo/bar").with({
+      should contain_homebrew__anytap(title).with({
         "ensure" => "present",
-        "url"    => "git@git.example.com:foo/homebrew-bar.git"
+        "url"    => url
       })
+    end
+
+    it { should contain_class("homebrew") }
+    it { should contain_homebrew__tap("telemachus/anytap") }
+    it { should contain_package("brew-any-tap").that_requires("Homebrew::Tap[telemachus/anytap]") }
+    it do
+      should contain_homebrew_anytap(title).with({
+        "ensure" => "present",
+        "url"    => url
+      }).that_requires("Package[brew-any-tap]")
     end
   end
 
@@ -25,14 +37,14 @@ describe "homebrew::anytap" do
     let(:params) do
       {
         :ensure => "absent",
-        :url => "git@git.example.com:foo/homebrew-bar.git"
+        :url    => url
       }
     end
 
     it do
-      should contain_homebrew__anytap("foo/bar").with({
+      should contain_homebrew__anytap(title).with({
         "ensure" => "absent",
-        "url"    => "git@git.example.com:foo/homebrew-bar.git"
+        "url"    => url
       })
     end
   end
