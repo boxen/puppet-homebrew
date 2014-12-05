abc38c5de108790807c0cf0c3f9ccdaa625c4692
David Goodlad <david@goodlad.net>
2014-12-05 16:38:18 +1100

    Use boxen's download url base for bottles by default

--- lib/facter/homebrew_bottle_url.rb
+++ lib/facter/homebrew_bottle_url.rb
@@ -3,5 +3,5 @@ Facter.add(:homebrew_bottle_url) do
 
   has_weight 1
 
-  setcode { "#{Facter.value(:boxen_s3_bucket)}.#{Facter.value(:boxen_s3_host)}/homebrew" }
+  setcode { "#{Facter.value(:boxen_download_url_base)}/homebrew" }
 end
