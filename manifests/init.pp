3e0fc912aae9744f467e194bff0b62018b9751d0
David Goodlad <david@goodlad.net>
2014-12-11 20:10:53 +1100

    Don't do a shallow clone, it causes issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -56,7 +56,7 @@ class homebrew(
     command => "git init -q &&
                 git config remote.origin.url https://github.com/${repo} &&
                 git config remote.origin.fetch master:refs/remotes/origin/master &&
-                git fetch origin master:refs/remotes/origin/master -n --depth=1 &&
+                git fetch origin master:refs/remotes/origin/master -n &&
                 git reset --hard origin/master",
     cwd     => $installdir,
     user    => $::boxen_user,
