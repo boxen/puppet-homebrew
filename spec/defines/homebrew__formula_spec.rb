af3b73658a33c02e622e67dc9fd96f92d836cd58
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-03-26 08:27:48 +1100

    Fix spec path to include Homebrew path

--- spec/defines/homebrew__formula_spec.rb
+++ spec/defines/homebrew__formula_spec.rb
@@ -4,7 +4,7 @@ describe "homebrew::formula" do
   let(:facts) { default_test_facts }
   let(:title) { "clojure" }
 
-  let(:tapdir) { "#{facts[:homebrew_root]}/Library/Taps/boxen/homebrew-brews" }
+  let(:tapdir) { "#{facts[:homebrew_root]}/Homebrew/Library/Taps/boxen/homebrew-brews" }
 
   context "with source provided" do
     let(:params) do
